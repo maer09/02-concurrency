ec72d2fac3973c6d4b3d5e9076ff6b3308c2d194
maer09 <maer09@users.noreply.github.com>
2024-05-05 10:45:07 +0800

    使用rwlock并发

---
+++ src/metrics.rs
@@ -0,0 +1,54 @@
+// metrics data structure
+// 基本功能： inc/dec/snapshot
+
+use anyhow::{anyhow, Result};
+use std::{
+    collections::HashMap,
+    fmt,
+    sync::{Arc, RwLock},
+};
+
+#[derive(Debug, Clone)]
+pub struct Metrics {
+    data: Arc<RwLock<HashMap<String, i64>>>,
+}
+
+impl Metrics {
+    pub fn new() -> Self {
+        Metrics {
+            data: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
+
+    pub fn inc(&self, key: impl Into<String>) -> Result<()> {
+        // 查不到则用缺省值
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
+        let counter = data.entry(key.into()).or_insert(0);
+        *counter += 1;
+        Ok(())
+    }
+
+    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
+        Ok(self
+            .data
+            .read()
+            .map_err(|e| anyhow!(e.to_string()))?
+            .clone())
+    }
+}
+
+impl Default for Metrics {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl fmt::Display for Metrics {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let data = self.data.read().map_err(|_e| fmt::Error {})?;
+        for (key, value) in data.iter() {
+            writeln!(f, "{}: {}", key, value)?;
+        }
+        Ok(())
+    }
+}
